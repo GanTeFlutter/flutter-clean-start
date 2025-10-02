f783a453f71851681117414db7876426964195c2
GanTeFlutter <rutinislerhes.0@gmail.com>
2025-10-02 18:53:52 +0300

    env

--- lib/product/init/app_initialize.dart
+++ lib/product/init/app_initialize.dart
@@ -1,13 +1,17 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_base_start/product/service/service_locator.dart';
 
 @immutable
 final class AppInitialize {
   Future<void> make() async {
+    
     WidgetsFlutterBinding.ensureInitialized();
     // await Firebase.initializeApp(
     //   options: DefaultFirebaseOptions.currentPlatform,
     // );
+    setupLocator();
+
   }
 }
